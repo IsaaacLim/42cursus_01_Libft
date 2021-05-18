33adbb836c5ec3b989d8a2de569ed98c7a3e986b
isaaclim <isaaclimjj@yahoo.com>
2021-05-18 23:16:35 +0800

    upgraded memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -4,11 +4,13 @@ void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*str;
 
+	if (!*s)
+		return (NULL);
 	str = (unsigned char *)s;
 	while (n-- > 0)
 	{
 		if (*str++ == (unsigned char)c)
-			return (str - 1);
+			return ((void*)(str - 1));
 	}
 	return (NULL);
 }
