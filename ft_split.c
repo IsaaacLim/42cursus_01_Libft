604b7ad9c90d916fb9644904d19e4f180af46d9c
Jinq jia Lim <jinlim@u90z04s03.42kl.edu.my>
2021-05-20 00:49:45 +0800

    ft_split left norm

--- ft_split.c
+++ ft_split.c
@@ -1,7 +1,6 @@
 #include "libft.h"
-#include "stdio.h"//remove
 
-int word_count(char const *s, char c)
+int	word_count(char const *s, char c)
 {
 	int	i;
 	int	word_count;
@@ -14,27 +13,24 @@ int word_count(char const *s, char c)
 	while (s[i])
 	{
 		if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
-				word_count++;
+			word_count++;
 		i++;
 	}
 	return (word_count);
 }
 
-
 char	**ft_split(char const *s, char c)
 {
-	int		words;
 	char	**arr;
 	char	*str;
-	int 	len;
-	char 	dsize;
-	
-	dsize = a;
+	int		words;
+	int		count;
+	int		len;
+
 	words = word_count(s, c);
 	arr = (char **)malloc(words * sizeof(char *) + 1);
 	if (!arr)
 		return (NULL);
-	int count;
 	count = 0;
 	while (count < words)
 	{
@@ -43,28 +39,19 @@ char	**ft_split(char const *s, char c)
 			len++;
 		if (len)
 		{
-			str = ft_calloc(len, dsize); //inlcuded + 1
+			str = ft_calloc(len + 1, 'a');
 			if (!str)
 			{
 				free(arr);
 				return (NULL);
 			}
-			arr[count] = ft_strlcpy(str, s, len);
+			arr[count] = ft_memcpy(str, s, len);
 			count++;
 			s = &s[len];
 		}
 		while (*s == c)
 			s++;
-	
 	}
+	arr[count] = NULL;
 	return (arr);
 }
-
-/*
-int main ()
-{
-	char *word = "ZZZZIsaacZZZIsaacZZZIsaacZZZ";
-	char deli = 'Z';
-	ft_split(word, deli);
-	//free(ft_split(word, deli));
-} */
