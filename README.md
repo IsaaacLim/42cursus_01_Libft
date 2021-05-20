d188553db9850c7251d0123e43fe37650e8e5c48
isaaclim <isaaclimjj@yahoo.com>
2021-05-20 14:39:44 +0800

    putchar_fd done

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ ft_strtrm.c - Last line
 ft_split.c - should work fine. Reduce function length
 ft_itoa.c - done
 ft_strmapi.c - norm
+ft_putchar_fd - check norm
--- libft.h
+++ libft.h
@@ -19,6 +19,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
+void	ft_putchar_fd(char c, int fd);
 char	**ft_split(char const *s, char c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
