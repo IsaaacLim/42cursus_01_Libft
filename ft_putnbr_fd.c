a83cae039a6855c96d8e033d0db90b1004b3edd2
isaaclim <isaaclimjj@yahoo.com>
2021-05-20 16:24:54 +0800

    updated putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -2,9 +2,18 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char *s;
-
-	s = ft_itoa(n);
-	ft_putstr_fd(s, fd);
-	free(s);
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		if (n == -2147483648)
+		{
+			ft_putchar_fd('2', fd);
+			ft_putnbr_fd(147483648);
+			return ;
+		}
+		n *= -1;
+	}
+	if (n > 9)
+		ft_putnbr_fd(n /10, fd);
+	ft_putchar_fd(n % 10 + '0', fd);
 }
